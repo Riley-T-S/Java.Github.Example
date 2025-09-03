d9455e22272a7ef8ed39301d2f7e6c1890675b46
GarrisonMoore <mooreg444@gmail.com>
2025-09-03 14:34:50 -0700

    resolved a merge error

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Github!");
         System.out.println("This is an error");
+        System.out.println("resolved merge conflict");
     }
 }
